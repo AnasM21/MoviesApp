f3fc7c1192b2b2e98a268f9d320c539741173c64
BENERRAZAM_MOHAMMADI <mohammadi_anas@emsi-edu.ma>
2022-04-09 23:34:11 +0000

    first commi

---
+++ API/TMDBApi.js
@@ -0,0 +1,15 @@
+
+const API_TOKEN = "fe9851a9a32dc0201b962d694e66a8fa";
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+
+  }
+  // API/TMDBApi.js
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+  }
+  
